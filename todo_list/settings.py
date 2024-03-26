a9747d5974768fbcb1bc6efb0c881fb7376e4561
Edward Kaboggoza <eddiegoza@gmail.com>
2024-03-26 15:27:31 +0300

    reconfigure the databse to new updates from django.

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -120,3 +120,8 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# settings.py
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
